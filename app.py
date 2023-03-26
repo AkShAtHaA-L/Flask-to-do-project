810962dbfeca62229661ae380a3915495cdb3c4d
Akshatha.L <akshatha.26997@gmail.com>
2023-03-26 15:53:31 +0530

    to-do actions

--- app.py
+++ app.py
@@ -7,7 +7,26 @@ TASKS = []
 @app.route("/", methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        TASKS.append(request.form['new_task'])
+        if 'action' not in request.form:
+            print(request.method)
+        
+        if request.form['action'] == 'add_new_task':
+            TASKS.append(request.form['new_task'])
+        
+        else:
+            task_id = int(request.form['task_id'])
+            if request.form['action'] == 'Completed':
+                del TASKS[task_id]
+            
+            elif request.form['action'] == "Delete":
+                del TASKS[task_id]
+            
+            elif request.form['action'] == "edited_task":
+                task_id = int(request.form['task_id'])
+                new_task = request.form['new_task']
+
+                TASKS[task_id] = new_task
+        
         return redirect(url_for('index'))
     else:
         return render_template('index.html',
