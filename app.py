1015cc5bd4a486818ea32787962d3c4ae157aade
Akshatha.L <akshatha.26997@gmail.com>
2023-03-26 12:38:43 +0530

    modal box and list view

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+TASKS = []
+
+@app.route("/", methods=['GET','POST'])
 def index():
-    return render_template('index.html')
-
-@app.route("/add-task")
-def add_task():
-    return render_template('add-task.html')
-
+    if request.method == 'POST':
+        TASKS.append(request.form['new_task'])
+        return redirect(url_for('index'))
+    else:
+        return render_template('index.html',
+                           tasks=TASKS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
